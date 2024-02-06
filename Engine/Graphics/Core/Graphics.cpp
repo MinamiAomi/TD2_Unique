4829095f19b8901f510b11aa152f442edb231fb9
MinamiAomi <k022g0040@g.neec.ac.jp>
2024-02-07 01:43:38 +0900

    ひっかかってそうなRootSignatureを削除

--- Engine/Graphics/Core/Graphics.cpp
+++ Engine/Graphics/Core/Graphics.cpp
@@ -192,7 +192,7 @@ void Graphics::CreateDevice() {
 }
 
 void Graphics::CreateDynamicResourcesRootSignature() {
-    D3D12_ROOT_SIGNATURE_DESC dynamicResourcesRootSignatureDesc{};
+    /*D3D12_ROOT_SIGNATURE_DESC dynamicResourcesRootSignatureDesc{};
     dynamicResourcesRootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_NONE;
     dynamicResourcesRootSignatureDesc.Flags |= D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
     dynamicResourcesRootSignatureDesc.Flags |= D3D12_ROOT_SIGNATURE_FLAG_CBV_SRV_UAV_HEAP_DIRECTLY_INDEXED;
@@ -202,5 +202,5 @@ void Graphics::CreateDynamicResourcesRootSignature() {
     rootParameters[0].InitAsConstantBufferView(0);
     dynamicResourcesRootSignatureDesc.pParameters = rootParameters;
     dynamicResourcesRootSignatureDesc.NumParameters = _countof(rootParameters);
-    dynamicResourcesRootSignature_.Create(L"RootSignature DynamicResources", dynamicResourcesRootSignatureDesc);
+    dynamicResourcesRootSignature_.Create(L"RootSignature DynamicResources", dynamicResourcesRootSignatureDesc);*/
 }
